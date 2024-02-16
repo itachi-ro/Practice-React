95583f5f37019f445261de074b1a146010bf555f
Rohan Nautiyal <rohannautiyal122000@gmail.com>
2024-02-16 23:12:20 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import First from "./first"
+
+function App() {
+  return (
+    <>
+      <First/>
+      <h3>Hello React</h3>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/first.jsx
@@ -0,0 +1,7 @@
+function First(){
+    return(
+        <h3>First add</h3>
+    )
+}
+
+export default First
